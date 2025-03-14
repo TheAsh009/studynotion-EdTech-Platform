a5057517078a3808d6524342efceafdda2df7168
Ashitosh Patil <Ashitoshpatil27@gmail.com>
2025-03-14 13:57:33 -0800

    All models(Schema) createdcsuccessfully

---
+++ models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  confirmPassword: {
+    type: String,
+    required: true,
+  },
+  accountType: {
+    type: String,
+    enum: ["Student", "Admin", "Instructor"],
+  },
+  additionalDetails: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Profile",
+  },
+  courses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
+  image: {
+    type: String,
+    required: true,
+  },
+  courseProgress: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "CourseProgress",
+    },
+  ],
+});
+
+module.exports = mongoose.model("User", userSchema);

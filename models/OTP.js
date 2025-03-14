7a3faa97e0471c6ba498ee84319552bb865808e8
Ashitosh Patil <Ashitoshpatil27@gmail.com>
2025-03-14 14:20:11 -0800

    implement nodemailer to send email verification mail to user and also add pre save hook

--- models/OTP.js
+++ models/OTP.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
-const otpSchema = new mongoose.Schema({
+const OTPSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -13,6 +14,28 @@ const otpSchema = new mongoose.Schema({
     type: Date,
     default: Date.now(),
   },
+});
+
+// a function to send emails
+async function sendVerificationEmail(email, otp) {
+  try {
+    const mailResponse = await mailSender(
+      email,
+      "Verification Email From StudyNotion",
+      otp
+    );
+    console.log("Email Send Successfully", mailResponse);
+  } catch (error) {
+    console.log(
+      "Error Occured in the sendVerification Mail in OTP Model",
+      error.message
+    );
+    throw error;
+  }
+}
+
+OTPSchema.pre("save", async function (next) {
+  await sendVerificationEmail(this.email, this.otp);
 });
 
 module.exports = mongoose.model("OTP", otpSchema);
